a279ddbfdb2f4d2e2d0e62800aa2ca989d76004c
ndebuhr <ndebuhr@protonmail.com>
2019-02-12 22:15:25 -0500

    Clean up JavaScript code formatting

--- src/test/resources/01-init.js
+++ src/test/resources/01-init.js
@@ -1,5 +1,5 @@
 // Copyright 2019 XEBIALABS - MIT License
-db = db.getSiblingDB('mongodbtest')
-db.adminCommand('listDatabases')
-db.users.save( {username:"xldeploy"} )
-db.users.find()
+db = db.getSiblingDB("mongodbtest");
+db.adminCommand("listDatabases");
+db.users.save( {username:"xldeploy"} );
+db.users.find();
